3718df40bbc2415b6ff0911a7b56eb4474e353cf
Allen <tracy1kobe@gmail.com>
2014-08-26 00:04:21 +0800

    publish

---
+++ wwwroot/js/script.js
@@ -0,0 +1,72 @@
+//prevent dropdown collapse accidentally closed
+$(function() {
+    window.prettyPrint && prettyPrint()
+    $(document).on('click', '.yamm .dropdown-menu', function(e) {
+        e.stopPropagation();
+    });
+});
+//enable dropdown
+$(document).ready(function() {
+    //enable dropdown on hover
+    //also add hovered class to hovered dropdown menu
+    $('.navbar .dropdown-hover').hover(function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).delay(150).slideDown(200);
+        $(this).children("a").addClass("hovered");
+        $('.dropdown').removeClass("open");
+    }, function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).delay(0).slideUp(1);
+        $(this).children("a").removeClass("hovered");
+    });
+
+    //enable lazy load
+    $("img.lazy").lazyload({
+        effect: "fadeIn"
+    });
+});
+
+//vertical align modal
+function adjustModalMaxHeightAndPosition() {
+    $('.modal').each(function() {
+        if ($(this).hasClass('in') == false) {
+            $(this).show(); /* Need this to get modal dimensions */
+        }
+        ;
+        var contentHeight = $(window).height() - 60;
+        var headerHeight = $(this).find('.modal-header').outerHeight() || 2;
+        var footerHeight = $(this).find('.modal-footer').outerHeight() || 2;
+
+        $(this).find('.modal-content').css({
+            'max-height': function() {
+                return contentHeight;
+            }
+        });
+
+        $(this).find('.modal-body').css({
+            'max-height': function() {
+                return (contentHeight - (headerHeight + footerHeight));
+            }
+        });
+
+        $(this).find('.modal-dialog').css({
+            'margin-top': function() {
+                return -($(this).outerHeight() / 2);
+            },
+            'margin-left': function() {
+                return -($(this).outerWidth() / 2);
+            }
+        });
+        if ($(this).hasClass('in') == false) {
+            $(this).hide(); /* Hide modal */
+        }
+        ;
+    });
+}
+;
+$(window).resize(adjustModalMaxHeightAndPosition).trigger("resize");
+
+//auto calculate the top banner height
+$('.navbar').affix({
+    offset: {
+        top: $('.top-banner').height()
+    }
+});
