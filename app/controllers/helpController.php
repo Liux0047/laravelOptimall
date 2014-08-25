a1ae99360ff08eb2f59ca737c602fa745fd9865d
Allen <tracy1kobe@gmail.com>
2014-08-26 00:00:49 +0800

    initial publish

---
+++ app/controllers/helpController.php
@@ -0,0 +1,22 @@
+<?php
+
+class HelpController extends BaseController {
+    /*
+      |--------------------------------------------------------------------------
+      | Default Home Controller
+      |--------------------------------------------------------------------------
+      |
+      | You may wish to use controllers instead of, or in addition to, Closure
+      | based routes. That's great! Here is an example controller method to
+      | get you started. To route to this controller, just add the route:
+      |
+      |	Route::get('/', 'HomeController@showWelcome');
+      |
+     */
+
+    public function showHelpPage() {
+        $params['pageTitle'] = "目光之城帮助";
+        return View::make('help', $params);
+    }
+
+}
---
+++ app/views/components/top-banner.blade.php
@@ -0,0 +1,35 @@
+<div class="top-banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-8 col-xs-8">
+                <div class="logo">
+                    <a href="/optimall/" title="Return to the homepage">
+                        <img src="{{ asset('images/optimall.png') }}" alt="Optimall logo" />
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-1 col-xs-1">
+                <div class="row">
+                    <div class="col-md-3 optimall-promises">                                
+                        <h6><i class="fa fa-check-square-o fa-lg"></i> Best Value for Money</h6>
+                    </div>
+                    <div class="col-md-3 optimall-promises">
+                        <h6><i class="fa fa-check-square-o fa-lg"></i> 10-Day Exchange</h6>
+                    </div>
+                    <div class="col-md-3 optimall-promises">
+                        <h6><i class="fa fa-check-square-o fa-lg"></i> Cash on Delivery</h6>
+                    </div>
+                    <div class="col-md-3 optimall-promises">
+                        <h6><i class="fa fa-check-square-o fa-lg"></i> Secured Checkout</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-3 col-xs-3 cart-icon-ontainer">
+                <a href="/optimall/shopping-cart.php" >
+                    <img src="{{ asset('images/cart-icon.png') }}">
+                </a>
+                <span class="badge">1</span>  
+            </div>
+        </div>
+    </div>
+</div>
