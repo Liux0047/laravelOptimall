66d06c3bd9fbd8916ef5f7407122f69d6bbbdd68
Allen <tracy1kobe@gmail.com>
2014-09-02 16:27:10 +0800

    add errror msg for validator

--- app/views/components/page-frame/message-bar.blade.php
+++ app/views/components/page-frame/message-bar.blade.php
@@ -1,13 +1,28 @@
 @if(Session::has('status'))
 <div class="alert alert-success alert-dismissable fade in">
-    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-    <i class="fa fa-check-circle fa-lg"></i> 
-    {{ Session::get('status') }}
+	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+	<i class="fa fa-check-circle fa-lg"></i> 
+	{{ Session::get('status') }}
+</div>
+@elseif (Session::has('warning'))
+<div class="alert alert-warning alert-dismissable fade in">
+	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+	<i class="fa fa-exclamation-circle fa-lg"></i> 
+	{{ Session::get('error') }}
 </div>
 @elseif (Session::has('error'))
-<div class="alert alert-warning alert-dismissable fade in">
-    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-    <i class="fa fa-exclamation-circle fa-lg"></i> 
-    {{ Session::get('error') }}
+<div class="alert alert-error alert-dismissable fade in">
+	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+	<i class="fa fa-exclamation-circle fa-lg"></i> 
+	{{ Session::get('error') }}
 </div>
+@elseif ($errors->has())
+<div class="alert alert-danger alert-dismissable fade in">
+	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+	<i class="fa fa-exclamation-circle fa-lg"></i> 
+	@foreach ($errors->all() as $error)
+	<div>{{ $error }}</div>
+	@endforeach
+</div>
+@else
 @endif
