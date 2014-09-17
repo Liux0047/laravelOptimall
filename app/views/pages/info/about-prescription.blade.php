6cf1ed0e2eda4e852dd48ca8a18fad871d8696f4
Allen <tracy1kobe@gmail.com>
2014-09-17 12:39:47 +0800

    center img

--- app/views/pages/info/about-prescription.blade.php
+++ app/views/pages/info/about-prescription.blade.php
@@ -8,10 +8,10 @@
 
 @section ('content')
 <div id="about_prescription">	
-	<div class="container">
+	<div class="container img-center">
 		<img src="{{ asset(Config::get('optimall.lazyloadImg')) }}" 
 		data-original="{{ asset('images/info/about-prescription/about-prescription.jpg') }}" 
-		class="lazy">
+		class="lazy img-center">
 	</div>
 </div>
 @stop
--- app/views/pages/info/measure-pupil-distance.blade.php
+++ app/views/pages/info/measure-pupil-distance.blade.php
@@ -8,7 +8,7 @@
 @section ('content')
 <div id="tips-container">
 	<div id="" class="container">
-		{{ HTML::image('images/info/tips/tip-1.jpg')}}
+		{{ HTML::image('images/info/tips/tip-1.jpg', '', array('class'=>'img-center'))}}
 	</div>
 </div>
 @stop
