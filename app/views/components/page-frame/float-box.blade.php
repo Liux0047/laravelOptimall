869a7f152479a7fd92979b8b807c066124f8f6f4
Allen <tracy1kobe@gmail.com>
2014-10-09 20:12:51 +0800

    float box

--- app/views/components/page-frame/float-box.blade.php
+++ app/views/components/page-frame/float-box.blade.php
@@ -3,6 +3,7 @@
         <i class="fa fa-user fa-2x"></i>
     </div>
     <div class="float-box">
-
+        <p>请扫描下方微信二维码</p>
+        {{ HTML::image('images/icons/wechat.jpg') }}
     </div>
 </div>
