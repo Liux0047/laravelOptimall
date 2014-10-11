32ecdbb7fbc938f046cc9b77a5a19939923c1081
Allen <tracy1kobe@gmail.com>
2014-10-11 15:59:27 +0800

    carousel integration done

--- app/views/pages/index.blade.php
+++ app/views/pages/index.blade.php
@@ -8,6 +8,58 @@
 @stop
 
 @section ('content')
+
+<section id="feature_slider" class="lol">
+    <!--
+        Each slide is composed by <img> and .info
+        - .info's position is customized with css in index.css
+        - each <img> parallax effect is declared by the following params inside its class:
+
+        example: class="asset left-472 sp600 t120 z3"
+        left-472 means left: -472px from the center
+        sp600 is speed transition
+        t120 is top to 120px
+        z3 is z-index to 3
+        Note: Maintain this order of params
+
+        For the backgrounds, you can combine from the bgs folder :D
+    -->
+    <article class="slide" id="showcasing" style="background-image: url('{{  asset('images/carousel/backgrounds/landscape.png') }}');">
+        <img class="asset left-30 sp600 t60 z1" src="{{ asset('images/carousel/slides/scene1/macbook.png') }}" />
+        <div class="info">
+            <h2>第一张图片</h2>
+        </div>
+    </article>
+    <article class="slide" id="ideas" style="background-image: url('{{  asset('images/carousel/backgrounds/aqua.jpg') }}');">
+        <div class="info">
+            <h2>第二张图片</h2>
+        </div>
+        <img class="asset left-480 sp600 t180 z1" src="{{ asset('images/carousel/slides/scene2/left.png') }}" />
+        <img class="asset left-210 sp600 t133 z2" src="{{ asset('images/carousel/slides/scene2/middle.png') }}" />
+        <img class="asset left60 sp600 t180 z1" src="{{ asset('images/carousel/slides/scene2/right.png') }}" />
+    </article>
+    <article class="slide" id="tour" style="background-image: url('{{  asset('images/carousel/backgrounds/color-splash.jpg') }}');">
+        <img class="asset left-472 sp650 t110 z3" src="{{ asset('images/carousel/slides/scene3/ipad.png') }}" />
+        <img class="asset left-365 sp600 t170 z4" src="{{ asset('images/carousel/slides/scene3/iphone.png') }}" />
+        <img class="asset left-350 sp450 t35 z2" src="{{ asset('images/carousel/slides/scene3/desktop.png') }}" />
+        <img class="asset left-185 sp550 t120 z1" src="{{ asset('images/carousel/slides/scene3/macbook.png') }}" />
+        <div class="info">
+            <h2>完全是 使用多设备 的</h2>
+            <a href="features.html">浏览产品</a>
+        </div>
+    </article>
+    <article class="slide" id="responsive" style="background-image: url('{{  asset('images/carousel/backgrounds/indigo.jpg') }}');">
+        <img class="asset left-472 sp600 t60 z3" src="{{ asset('images/carousel/slides/scene4/html5.png') }}" />
+        <img class="asset left-190 sp500 t60 z2" src="{{ asset('images/carousel/slides/scene4/css3.png') }}" />
+        <div class="info">
+            <h2>
+                多设备 <strong>兼容的网页</strong>
+                主题
+            </h2>
+        </div>
+    </article>
+</section>
+
 <div id="index-carousel" class="carousel slide">                        
     <!-- Indicators -->
     <ol class="carousel-indicators">                
