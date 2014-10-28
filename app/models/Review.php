fa71845bdb3a3b3d5e6026591f6862b7c279a6d9
Allen <tracy1kobe@gmail.com>
2014-10-29 00:15:40 +0800

    group by bug fix

--- app/models/Review.php
+++ app/models/Review.php
@@ -37,13 +37,13 @@ class Review extends Eloquent
      */
     public function scopeOfModel($query, $modelId)
     {
-        return $query->select('review.review_id', 'review.title', 'review.content', 'review.created_at',
-            'review.design_rating', 'review.comfort_rating', 'review.quality_rating', 'review.order_line_item_id', 'member.nickname',
+        return $query->select('review.*', 'member.nickname',
             DB::raw('count(thumb_up.thumb_up_id) AS thumb_ups '))
             ->join('order_line_item', 'order_line_item.order_line_item_id', '=', 'review.order_line_item_id')
             ->join('product', 'product.product_id', '=', 'order_line_item.product_id')
             ->join('member', 'order_line_item.member_id', '=', 'member.member_id')
             ->leftJoin('thumb_up', 'thumb_up.review_id', '=', 'review.review_id')
+            ->groupBy('review.review_id')
             ->where('product.model_id', '=', $modelId);
     }
 }
