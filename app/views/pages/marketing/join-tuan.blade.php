cbd0d24e0770c2ac307b0f58c9d85d53ce01d28f
Allen <tracy1kobe@gmail.com>
2015-02-28 13:31:49 +0800

    UI fix

--- app/views/pages/marketing/join-tuan.blade.php
+++ app/views/pages/marketing/join-tuan.blade.php
@@ -1,45 +1,43 @@
 @extends ('layouts.tuan-base')
 
 @section('content')
-<div class="container">
-    <h4>加入团购活动</h4>
-    @if (Session::has('error'))
-        <div class="alert alert-warning fade in">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-            <i class="fa fa-exclamation-circle"></i>
-            {{ Session::get('error') }}
-        </div>
-    @endif
-    <hr>
-    {{ Form::open(array('action' => 'MarketingController@postJoinTuan', 'class' => 'form-horizontal')) }}
+    <div class="container">
+        <h4>加入团购活动</h4>
+        @if (Session::has('error'))
+            <div class="alert alert-warning fade in">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                <i class="fa fa-exclamation-circle"></i>
+                {{ Session::get('error') }}
+            </div>
+        @endif
+        <hr>
+        {{ Form::open(array('action' => 'MarketingController@postJoinTuan')) }}
 
-    <div class="form-group">
-        <label for="phone_number" class="col-sm-2 control-label">请输入手机号码</label>
+        <div class="form-group">
+            <label for="phone_number">请输入手机号码</label>
 
-        <div class="col-sm-10">
             <input type="text" class="form-control" value="" maxlength="11" name="phone_number"
-                   id="phone_number" />
+                   id="phone_number"/>
         </div>
+
+
+        <p class="text-center">
+            <button class="btn btn-danger" type="submit" disabled id="submit_btn">获取验证码</button>
+        </p>
+
+        {{ Form::close() }}
+
     </div>
-
-
-    <p class="text-center">
-        <button class="btn btn-danger" type="submit" disabled id="submit_btn">获取验证码</button>
-    </p>
-
-    {{ Form::close() }}
-
-</div>
 @stop
 
 @section('javascript')
-<script type="text/javascript">
-    $('#phone_number').on('input',function(e){
-        if ($(this).val().length == 11) {
-            $('#submit_btn').attr('disabled', false);
-        } else {
-            $('#submit_btn').attr('disabled', true);
-        }
-    });
-</script>
+    <script type="text/javascript">
+        $('#phone_number').on('input', function (e) {
+            if ($(this).val().length == 11) {
+                $('#submit_btn').attr('disabled', false);
+            } else {
+                $('#submit_btn').attr('disabled', true);
+            }
+        });
+    </script>
 @stop
--- app/views/pages/marketing/verify-tuan.blade.php
+++ app/views/pages/marketing/verify-tuan.blade.php
@@ -1,44 +1,41 @@
 @extends ('layouts.tuan-base')
 
 @section('content')
-<div class="container">
-    <h4>验证</h4>
-    @if (Session::has('error'))
-        <div class="alert alert-warning fade in">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-            <i class="fa fa-exclamation-circle"></i>
-            {{ Session::get('error') }}
-        </div>
-    @endif
-    <hr>
-    {{ Form::open(array('action' => 'MarketingController@postVerifyTuan', 'class' => 'form-horizontal')) }}
+    <div class="container">
+        <h4>验证</h4>
+        @if (Session::has('error'))
+            <div class="alert alert-warning fade in">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                <i class="fa fa-exclamation-circle"></i>
+                {{ Session::get('error') }}
+            </div>
+        @endif
+        <hr>
+        {{ Form::open(array('action' => 'MarketingController@postVerifyTuan')) }}
 
-    <div class="form-group">
-        <label for="code" class="col-sm-2 control-label">请输入验证码</label>
+        <div class="form-group">
+            <label for="code">请输入验证码</label>
 
-        <div class="col-sm-10">
             <input type="text" class="form-control" value="" maxlength="6" name="code" id="code"/>
         </div>
+
+        <p class="text-center">
+            <button class="btn btn-danger" type="submit" disabled id="submit_btn">验证</button>
+        </p>
+
+        {{ Form::close() }}
+
     </div>
-
-    <p class="text-center">
-        <button class="btn btn-danger" type="submit" disabled id="submit_btn">验证</button>
-    </p>
-
-
-    {{ Form::close() }}
-
-</div>
 
 
 @section('javascript')
-<script type="text/javascript">
-    $('#code').on('input',function(e){
-        if ($(this).val()) {
-            $('#submit_btn').attr('disabled', false);
-        } else {
-            $('#submit_btn').attr('disabled', true);
-        }
-    });
-</script>
+    <script type="text/javascript">
+        $('#code').on('input', function (e) {
+            if ($(this).val()) {
+                $('#submit_btn').attr('disabled', false);
+            } else {
+                $('#submit_btn').attr('disabled', true);
+            }
+        });
+    </script>
 @stop
